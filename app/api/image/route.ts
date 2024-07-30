722b3a077cca99e567b71f052a0744198e209f2d
Om Pandey <mynameisompandey007@gmail.com>
2024-07-30 20:32:39 +0530

    post route changes

--- app/api/image/route.ts
+++ app/api/image/route.ts
@@ -1,100 +1,55 @@
-import { auth } from "@clerk/nextjs/server";
-import { NextResponse } from "next/server";
-// import OpenAI from 'openai'
-import fs from "fs";
-import { string } from "zod";
 import axios from "axios";
-import MonsterApiClient from "monsterapi";
-import monsterApi from '@api/monster-api';
-import dotenv from 'dotenv';
-import fetch from 'node-fetch';
-
-
-dotenv.config();
-
-// const key=process.env.OPENAI_API_KEY
-// const openai=new OpenAI( { apiKey: key });
-
-
+import { NextResponse } from "next/server";
 
 export async function POST(req: Request) {
-  const key= process.env.MONSTER_API_KEY;
-  const client =new MonsterApiClient(`${key}`)
+  const key = process.env.IMAGE_GEN_API_KEY;
+
+  const baseUrl = "https://modelslab.com/api/v6/realtime/text2img";
+
+  let options = {
+    method: "POST",
+    url: "https://modelslab.com/api/v6/realtime/text2img",
+    headers: {
+      "Content-Type": "application/json",
+    },
+  };
+  const body = {
+    key,
+    prompt:
+      "ultra realistic close up portrait ((beautiful pale cyberpunk female with heavy black eyeliner))",
+    negative_prompt: "bad quality",
+    width: "512",
+    height: "512",
+    safety_checker: false,
+    seed: null,
+    samples: 1,
+    base64: false,
+    webhook: null,
+    track_id: null,
+  };
+
   try {
-    const { userId } = auth();
-    const body = await req.json();
-    const { messages } = body;
+    const response = await axios.post(baseUrl, body, options);
+    const { status, fetch_result } = response.data;
+    let imageStatus = status;
+    let postResoponse;
 
-    console.log(messages);
+    while (imageStatus == "processing") {
+      await new Promise((resolve) => setTimeout(resolve, 2000));
 
-    const payload={
-      safe_filter: true,
-      prompt: 'cat swiming',
-      guidance_scale: 7.5,
-      aspect_ratio: 'portrait',
-      negprompt: 'deformed, bad anatomy, disfigured, poorly drawn face',
-      samples: 1,
-      seed: 2414,
-      steps: 15,
-      style: 'anime'
+      postResoponse = await axios.post(fetch_result, { key }, options);
+      const { status } = postResoponse.data;
+      if (status == "sucess") {
+        imageStatus = "sucess";
+      } else if (status == "error") {
+        throw new Error("Image generation failed");
+      }
     }
 
-    // const input : Record<string, any>={
-    //   prompt:'cat swiming'
-    // }
+    console.log(postResoponse?.data);
 
-    // if (!userId) {
-    //   return new NextResponse("Unauthorized user", { status: 401 });
-    // }
-    if (!key) {
-      return new NextResponse("openai key is not set", { status: 500 });
-    }
-
-    // if(!messages){
-    //   return new NextResponse('no message has been given',{status:400})
-    // }
-    // const response = await openai.images.generate({
-    //     model: "dall-e-3",
-    //     prompt: "a white siamese cat",
-    //     n: 1,
-    //     size: "1024x1024",
-    //   });
-
-
-
-
-    const url = 'https://api.monsterapi.ai/v1/generate/txt2img';
-    const options = {
-      method: 'POST',
-      headers: {
-        accept: 'application/json',
-        'content-type': 'application/json',
-        authorization: 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VybmFtZSI6ImIwOTVhYzdkZjU0MzA2ZjQyM2JlNzA0MWY0ZDllNDcyIiwiY3JlYXRlZF9hdCI6IjIwMjQtMDctMzBUMDQ6MTg6NDYuNjEzMTExIn0.iKNmfUHIgpLjukANRqWQB9gtKkqq9Jt7xFyzRrl-KjM'
-      },
-      body: JSON.stringify({
-        safe_filter: true,
-        aspect_ratio: 'portrait',
-        guidance_scale: 7.5,
-        negprompt: 'deformed, bad anatomy, disfigured, poorly drawn face',
-        prompt: 'cat swiming',
-        samples: 1,
-        seed: 2414,
-        steps: 15,
-        style: 'anime'
-      })
-    };
-    
-   const response= fetch(url, options)
-      .then(res => console.log(res.json()))
-      .catch(err => console.error('error:' + err));
-  
-// console.log(response.data)
-
-return new NextResponse('true')
-
-
+    return new NextResponse(response.data);
   } catch (error) {
-    console.log("[CONVERSATION ERROR]", error);
-    return new NextResponse(`Error:${error}`, { status: 500 });
+    console.log("Error", error);
   }
 }
