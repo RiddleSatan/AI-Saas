a636d754967134920026e8a33245dd47958f39fb
Om Pandey <mynameisompandey007@gmail.com>
2024-08-12 14:57:57 +0530

    next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {images: {
-    remotePatterns: [
-      {
-      
-        hostname: 'hatrabbits.com',
-       
-      },
-    ],
+
   },};
 
 
